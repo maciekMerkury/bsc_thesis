7d3caac068c87e936457198a0ca6aee2ca082102
maciekMerkury <maciekmerkury2@gmail.com>
2025-08-21 02:14:15 +0200

    NodeJS program

---
+++ benchmarks/static_http_server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 4000;
+const IP = '127.0.0.1';
+
+const html = fs.readFileSync("./static.html", "utf8");
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': Buffer.byteLength(html)
+    });
+    res.end(html);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(PORT, IP, () => {
+  console.log(`Server running at http://${IP}:${PORT}/`);
+});
+
